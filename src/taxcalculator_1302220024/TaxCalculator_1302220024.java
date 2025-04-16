cde62c88d2cef4ba584a813ea96f6b9ebda6de04
rookieraa <zaaraalzh@gmail.com>
2025-04-17 00:35:54 +0700

    first commit

---
+++ src/taxcalculator_1302220024/TaxCalculator_1302220024.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taxcalculator_1302220024;
+
+/**
+ *
+ * @author ASUS TM420IA
+ */
+public class TaxCalculator_1302220024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
